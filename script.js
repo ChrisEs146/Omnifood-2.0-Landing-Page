51ec8a35520c85dc67c1960b2c3b031d5d728a75
ChrisEs146 <chris.es146@gmail.com>
2022-06-02 20:23:49 -0600

    Add: Event handler for the mobile menu

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+// SELECTIONS
+
+const header = document.querySelector(".header");
+
+// Events
+header.addEventListener("click", (e) => {
+  const hamBtn = e.target.closest(".header__hamburger-menu");
+  if (!hamBtn) return;
+  header.classList.toggle("open");
+});
