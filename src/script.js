75c364d6b3e6a018e5bb590a1c0911ecd9e1ec27
ChrisEs146 <chris.es146@gmail.com>
2022-06-08 21:17:57 -0600

    Add: Sticky navigation functionality
    - The sticky navigation functionality was added
    using the Intersection Observer API.

--- src/script.js
+++ src/script.js
@@ -26,9 +26,32 @@ tabContainer.addEventListener("click", (e) => {
   tabBtns.forEach((e) => e.classList.remove("howto__step-tab--active"));
   //Applying active state to clicked tab
   tab.classList.add("howto__step-tab--active");
+  //Creating relation between tab number and step container
   const tabNumber = tab.dataset.tab;
   stepContainer.forEach((e) => e.classList.remove("howto__step-container--active"));
   document
     .querySelector(`.howto__step-container-${tabNumber}`)
     .classList.add("howto__step-container--active");
 });
+
+//Sticky Navigation
+const navHeight = navigation.clientHeight;
+const navOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+};
+
+const navCallback = (entries) => {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    hero.style.marginTop = `${navHeight}px`;
+    navigation.classList.add("sticky");
+  } else {
+    navigation.classList.remove("sticky");
+    hero.style.marginTop = `0px`;
+  }
+};
+
+const navObserver = new IntersectionObserver(navCallback, navOptions);
+navObserver.observe(header);
